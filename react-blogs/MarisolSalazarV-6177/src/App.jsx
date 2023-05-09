22ae35f345aca953136e3c242eb37a0a0807884d
Marisol Salazar <salazarvalenciamarisol@gmail.com>
2023-05-09 17:37:55 -0500

    firts commit with react-blog

---
+++ react-blogs/MarisolSalazarV-6177/src/App.jsx
@@ -0,0 +1,15 @@
+import { Blog } from './Components/Blog'
+import { Footer } from './Components/Footer';
+import { Header } from './Components/Header';
+
+function App() {
+  return(
+    <>
+    <Header />
+    <Blog />
+    <Footer />
+    </>
+  )
+}
+
+export default App;
