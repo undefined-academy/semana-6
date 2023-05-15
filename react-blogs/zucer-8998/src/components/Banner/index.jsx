6f392ea91349378953968440a4d95f6b8f9c5466
Julian Sucerquia <andresucerquia@gmail.com>
2023-05-15 14:04:26 -0500

    feat: Create Banner component

---
+++ react-blogs/zucer-8998/src/components/Banner/index.jsx
@@ -0,0 +1,19 @@
+import Layout from '../Layout'
+import './Banner.css'
+
+const Banner = () => (
+  <div className="banner">
+    <Layout className="banner-margin">
+      <div className="banner-title">
+        <h1 className="title-1">
+          Hi 👋,I&apos;m
+          <br />
+          Julián Sucerquia
+        </h1>
+        <span className="badge">Web Developer</span>
+      </div>
+    </Layout>
+  </div>
+)
+
+export default Banner
