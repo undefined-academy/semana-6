7ea3c2239f062d28477966391408706aa53a7ee3
Julian Sucerquia <andresucerquia@gmail.com>
2023-05-16 01:11:26 -0500

    feat: Create blog and article component

--- react-blogs/zucer-8998/src/components/index.jsx
+++ react-blogs/zucer-8998/src/components/index.jsx
@@ -2,5 +2,7 @@ import Header from './Header'
 import Footer from './Footer'
 import Banner from './Banner'
 import Layout from './Layout'
+import Blog from './Blog'
+import Article from './Article'
 
-export { Header, Footer, Banner, Layout }
+export { Header, Footer, Banner, Layout, Blog, Article }
