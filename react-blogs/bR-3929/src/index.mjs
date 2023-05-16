f7d75d8d76730fcd2a553cfda6407cd94a5e1a73
Brian <beerre20@gmail.com>
2023-05-16 19:32:41 -0300

    Corrigiendo el dayjs

--- react-blogs/bR-3929/src/index.mjs
+++ react-blogs/bR-3929/src/index.mjs
@@ -4,18 +4,22 @@ dayjs.extend(relativeTime);
 import es from "dayjs/locale/es.js";
 dayjs.locale(es);
 
-    function humanize(dates){
-        const now = dayjs()
-    
-        if(now.diff(dates, "year") < 1 && now.diff(dates, "month") >= 1){
-            return dayjs().format("MMMM DD");
-        }
-        else if(now.diff(dates, "year") > 1){
-            return dayjs().format("MMMM DD, YYYY");
-        }
-        else{
-        return "Publicado " + now.to(dates);
-        }
+function humanize(dates) {
+    const now = dayjs();
+  
+    if (now.diff(dates, "year") < 1 && now.diff(dates, "month") >= 1) {
+      const formattedDate = dayjs().format("MMMM DD");
+      return capitalizeFirstLetter(formattedDate);
+    } else if (now.diff(dates, "year") > 1) {
+      const formattedDate = dayjs().format("MMMM DD, YYYY");
+      return capitalizeFirstLetter(formattedDate);
+    } else {
+      return "Publicado " + now.to(dates);
     }
+  }
+  
+  function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
 
     export {humanize}
