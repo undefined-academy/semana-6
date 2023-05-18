8367fe8e570bae35f7ae3a3822dcffd0fc5a1b9f
Flor <florencia.ortiz096@gmail.com>
2023-05-18 16:12:16 -0300

    Blog creado con React

---
+++ react-blogs/Floor-3100/src/components/Nav.jsx
@@ -0,0 +1,14 @@
+const Nav = () => ( 
+        <nav className="navbar">
+            <ul>
+                <li>Inicio</li>
+                <li>Programacion</li>
+                <li>Lenguajes</li>
+                <li>Librerias</li>
+                <li>Actualidad</li>
+            </ul>
+            <input type="search" className="search" placeholder="Buscar" />
+        </nav>
+    );
+
+export default Nav
