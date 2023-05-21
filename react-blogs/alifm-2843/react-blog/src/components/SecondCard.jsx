d8ec239626563069610dcdb58d7e706da74e8027
ali <alianncomp@gmail.com>
2023-05-21 02:15:02 -0500

    docs: Adding files

---
+++ react-blogs/alifm-2843/react-blog/src/components/SecondCard.jsx
@@ -0,0 +1,22 @@
+function SecondCard() {
+    return (
+
+        <>
+            <section id="country"  className="card">
+                <img src="https://source.unsplash.com/sYffw0LNr7s" alt="country"/>
+                <h1>Country life 🏜️</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi tenetur laboriosam temporibus
+                    quibusdam impedit inventore enim? Alias placeat dolorum amet sint. Odit fugit sed adipisci.
+                    Molestiae distinctio vel harum enim?</p>
+                <span> Posted: <time dateTime="2022-07-23" className="dates"></time></span>
+            </section>
+
+            
+            
+
+        </>
+
+    )
+}
+
+export default SecondCard;
---
+++ react-blogs/alifm-2843/react-blog/src/components/ThirdCard.jsx
@@ -0,0 +1,15 @@
+function ThirdCard() {
+    return (
+        <>
+        <section id="mountain"  className="card">
+                <img src="https://source.unsplash.com/V7WkmXntA8M" alt="mountains"/>
+                <h1>Mountains ⛰️</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi tenetur laboriosam temporibus
+                    quibusdam impedit inventore enim? Alias placeat dolorum amet sint. Odit fugit sed adipisci.
+                    Molestiae distinctio vel harum enim?</p><span>Posted: <time dateTime="2022-01-01" className="dates"></time></span>
+            </section>
+        </>
+    )
+}
+
+export default ThirdCard;
