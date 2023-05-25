a20cfa692cd92073b4e82838a2c3b3cfb4c1bd4f
Pablo Rodriguez <pablo.armp@gmail.com>
2023-05-24 23:10:36 -0400

    Add react hooks practice life cycle class 4

--- react-blogs/PabloXD-4695/src/App.jsx
+++ react-blogs/PabloXD-4695/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
+import ParentComponent from "./components/ParentComponent";
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
       <Footer />
     </div>
   );
+
+  //In order to test the exercise of the class 4 you can enable de next code
+  //return <ParentComponent />;
 }
 
 export default App;
