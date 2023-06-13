f97280e65d397fdb42f5112ceb3428a2d86f0511
Eduardo <ejrb1234@gmail.com>
2023-06-13 18:41:51 -0400

    Adding Class3,Week6 - Exercise

---
+++ react-blogs/MrRedu-7518/intro-a-react/src/Card.jsx
@@ -0,0 +1,24 @@
+// import './Card.css' // ! No existe. Solo es ejemplo
+
+const articles = [
+    { title: 'Tepuy Kukenán', text: 'El tepuy Kukenán​ o Cuquenán es un tepuy en la parroquia Santa Elena de Uairén, municipio Gran Sabana, estado Bolívar, en la región Guayana, parte del país sudamericano de Venezuela. Posee una superficie estimada en 2185 hectáreas. Tiene una altitud de 2680 m y una longitud de 3 km' },
+    { title: 'Monte Roraima', text: 'El monte Roraima, también conocido como tepuy Roraima, cerro Roraima o simplemente Roraima, con 2810 m s. n. m., es el punto más alto de la cadena de mesetas tepuyes de la sierra de Pacaraima, en Venezuela.' },
+    { title: 'Gran sabana', text: 'La Gran Sabana es una región localizada al sureste de Venezuela, en el macizo de las Guayanas, también al Sur-este del Estado Bolívar, y que se extiende hasta la frontera con Brasil y Guyana.' },
+]
+
+export const Card = () => {
+
+    const listArticle = articles
+        .map(article =>
+            <div className="post">
+                <h1>{article.title}</h1>
+                <p>{article.text}</p>
+            </div>
+        )
+
+    return (
+        <>
+            {listArticle}
+        </>
+    )
+}
