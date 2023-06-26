d0d3e2f2f02d033ef1bc349b5194013b5bdf65d7
santiago01est <santiago01est@gmail.com>
2023-06-26 16:52:03 -0500

    ejercicio semana 6 intro a React

---
+++ react-blogs/santiagoestrada-6564/src/helpers/data.js
@@ -0,0 +1,34 @@
+const items = [
+    {
+      title: "ReelTalk a New App for cinema Lovers in 2023",
+      description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi tenetur laboriosam temporibus quibusdam impedit inventore enim?`,
+      date: "2023-06-24",
+      imageUrl: "https://firebasestorage.googleapis.com/v0/b/seventh-art-d7971.appspot.com/o/perfil%2Freeltalkapp%40gmail.comphoto.jpg?alt=media&token=d627a294-0a03-4b16-9271-f888856c38ff",
+    },
+    {
+      title: "Auto drive for your car in 2023", 
+      description: `the future is here, where the cars will be autonomous, and they will drive themselves, so you don't have to, you just have to drive, you don't have to worry about anything.`,
+      date: "2023-04-20",
+      imageUrl: "https://images.unsplash.com/photo-1519641471654-76ce0107ad1b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80",
+    },
+    {
+      title: "AI: Artificial Intelligence in 2023",
+      description: `AI is the future, and AI is the present, and AI is the future, and AI is the present, but the future is here, and the present is here, and the future is here, and the present is here`,
+      date: "2021-12-20",
+      imageUrl: "https://images.unsplash.com/photo-1620712943543-bcc4688e7485?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=765&q=80",
+    },
+    {
+      title: "Cats domain in 2023",
+      description: `the cats are the future, and the cats are the present, and the cats are the future, and the cats are the present, but the future is here, and the present is here, and the future is here, and the present is here`,
+      date: "2023-03-20",
+      imageUrl: "https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80",
+    },
+    {
+      title: "The Bootcamp fullstack in 2023",
+      description: `Undefined is a bootcamp, and it is the future, and it is the present, and it is the future, and it is the present, but the future is here, and the present is here, and the future is here, and the present is here`,
+      date: "2023-01-20",
+      imageUrl: "https://images.unsplash.com/photo-1531498860502-7c67cf02f657?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    },
+  ];
+  
+  export default items;
