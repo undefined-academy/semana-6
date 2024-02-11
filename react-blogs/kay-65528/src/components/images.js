ddc88f0ccb05d4e7028133611f5ff6a603522273
suru <garudahorror25@gmail.com>
2024-02-11 18:46:50 -0400

    small changes to the assets used in the blog

--- react-blogs/kay-65528/src/components/images.js
+++ react-blogs/kay-65528/src/components/images.js
@@ -1,28 +1,25 @@
-import crawl from '../assets/crawl.png';
-import dohna from '../assets/dohna_dohna.png';
-import dragon from '../assets/dragon_knight.png';
-import luffy from '../assets/luffy_sun_god.jpg';
+import image from '../assets/image_template.jpg';
 
 const collectionOfImages = [
   {
-    url: crawl,
-    description: 'Panel from the Goblin Slayer manga',
-    tags: ['Manga', 'Books', 'Eastern']
+    url: image,
+    description: 'Text description used for the this entry',
+    tags: ['Tag1', 'Tag2']
   },
   {
-    url: dohna,
-    description: 'Album Cover of the Dohna Dohna soundtrack',
-    tags: ['Music', 'Gaming', 'Eastern']
+    url: image,
+    description: 'Text description used for the this entry',
+    tags: ['Tag1', 'Tag2', 'Tag3']
   },
   {
-    url: dragon,
-    description: 'Fantasy example cover',
-    tags: ['Fantasy', 'Gaming', 'Western']
+    url: image,
+    description: 'Text description used for the this entry',
+    tags: ['Tag1']
   },
   {
-    url: luffy,
-    description: 'New image about the sun god nika from One Piece',
-    tags: ['Anime', 'Manga', 'Eastern']
+    url: image,
+    description: 'Text description used for the this entry',
+    tags: ['Tag1', 'Tag2', 'Tag3', 'Tag4']
   }
 ]
 
